cf7dd5d8e54f8455b6816840baf12b5fee6b8eb0
Vincent Samuel <chintuvincent@gmail.com>
2024-02-28 19:45:50 +0530

    added text in teams. fixed burger icon

--- src/Components/Team/teamData.js
+++ src/Components/Team/teamData.js
@@ -1,19 +1,21 @@
 export const teamData = [
     {
-        img : '/assets/T1.svg',
-        role : 'Founder and CEO',
-        name : 'Nikhil Goyal',
+        img: '/assets/T1.svg',
+        role: 'Founder and CEO',
+        name: 'Nikhil Goyal',
+        ex: '(Serial Entrepreneur)'
     },
     {
-        img : '/assets/T11.svg',
-        role : 'Co-Founder and CTO',
-        name : 'Anthony Raj',
+        img: '/assets/T11.svg',
+        role: 'Co-Founder and CTO',
+        name: 'Anthony Raj',
         ex: '(Ex-Microsoft and Cisco)'
     },
     {
-        img : '/assets/T2.svg',
-        role : 'COO',
-        name : 'Alejandro Giuffrida',
+        img: '/assets/T2.svg',
+        role: 'COO',
+        name: 'Alejandro Giuffrida',
+        ex: '(Former rector of the Champagnat University (Argentina) and president of the Association of Latin American and Caribbean Universities for Integration)'
     },
     // {
     //     img : '/assets/T3.svg',
